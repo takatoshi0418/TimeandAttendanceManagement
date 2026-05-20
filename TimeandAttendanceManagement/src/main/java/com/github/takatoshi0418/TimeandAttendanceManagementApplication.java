27606e99265d5cec1df70a5b9cfae9260248522c
takatoshi0418 <58429936+takatoshi0418@users.noreply.github.com>
2026-05-20 22:52:29 +0900

    first commit

---
+++ TimeandAttendanceManagement/src/main/java/com/github/takatoshi0418/TimeandAttendanceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.takatoshi0418;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeandAttendanceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeandAttendanceManagementApplication.class, args);
+	}
+
+}
---
+++ TimeandAttendanceManagement/src/test/java/com/github/takatoshi0418/TimeandAttendanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.takatoshi0418;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeandAttendanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
